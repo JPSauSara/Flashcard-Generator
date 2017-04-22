7b8d11943239adb227ff857f5c5b5dce7083ac31
Julia Saucedo <juliapaola@Julias-MacBook-Pro.local>
2017-04-22 08:41:45 -0500

    BasicCard - DONE

--- BasicCard.js
+++ BasicCard.js
@@ -3,22 +3,26 @@
 // This file should define a Node module that exports a constructor for creating basic flashcards
 // 		e.g.: module.exports = BasicCard;
 // The constructor should accept two arguments: front and back.
-
-
-function BasicCard () {
-	console.log("BasicCards are working!");
-
-// 	this.front = front;
-// 	this.back = back;
-};
-
 // The constructed object should have a front property that contains the text on the front of the card.
 // The constructed object should have a back property that contains the text on the back of the card.
 
 
+function BasicCard (front, back) {
+	if (this instanceof BasicCard) {
+		this.front = front;
+		this.back = back;
+	} else {
+		return new BasicCard (front, back);
+	}
 
+};
 
+// instanceof helps handle the removal of the "new" keyword when creating an instance of a class.
 
 module.exports = BasicCard;
+
+
+
+
 
 
