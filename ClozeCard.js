7349663dee1baf1a25f4993161912b954d1fba6b
Julia Saucedo <juliapaola@Julias-MacBook-Pro.local>
2017-04-22 09:00:32 -0500

    add: ClozeCard

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 // This file should define a Node module that exports a constructor for creating cloze-deletion flashcards, 
 // 		e.g.: module.exports = ClozeCard;
 // The constructor should accept two arguments: text and cloze.
@@ -9,3 +8,25 @@
 // The constructed object should have a fullText property that contains only the full text.
 // The constructor should throw or log an error when the cloze deletion does not appear in the input text.
 // Use prototypes to attach these methods, wherever possible.
+
+function ClozeCard (text, cloze) {
+	if (this instanceof ClozeCard) {
+		this.fulltext = text;
+		this.cloze = cloze;
+		this.partialtext = function {
+			if (this.text.includes(this.cloze)) {
+				return this.fullText.replace(this.cloze, "...");
+			} else {
+				console.log("Error:")
+			}
+		};
+
+	} else {
+		return new ClozeCard (text, cloze);
+	}
+
+};
+
+
+
+module.exports = ClozeCard;
